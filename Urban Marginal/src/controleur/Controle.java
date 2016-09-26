a4cf9a814aa15071d5207cefd4f011c7f68f2f7c
admin <admin@10.20.130.113>
2016-09-26 11:04:39 +0200

    Rendre visible la frame Arene

--- Urban Marginal/src/controleur/Controle.java
+++ Urban Marginal/src/controleur/Controle.java
@@ -5,6 +5,7 @@ import modele.Jeu;
 import modele.JeuServeur;
 import outils.connexion.ClientSocket;
 import outils.connexion.ServeurSocket;
+import vue.Arene;
 import vue.EntreeJeu;
 
 public class Controle {
@@ -14,16 +15,13 @@ public class Controle {
 	}	
 	private EntreeJeu frmEntreeJeu;
 	private Jeu leJeu;
+	private Arene frmArene;
 
 	
 	//Constructeur
 	public Controle(){
-		
 		this.frmEntreeJeu= new EntreeJeu(this);//création de la frame
-		frmEntreeJeu.setVisible(true);//rend visible la frame
-		
-
-		
+		frmEntreeJeu.setVisible(true);//rend visible la frame	
 	}
 	public void evenementVue(Object uneFrame, Object info){
 		if (uneFrame instanceof EntreeJeu){
@@ -38,6 +36,8 @@ public class Controle {
 			new ServeurSocket(this,6666);//this est l'instance actuelle de Controle.
 			leJeu= new JeuServeur(this);//Un jeu de type serveur va ainsi être créé.
 			frmEntreeJeu.dispose();//La frame d'entrée peut être fermée.
+			frmArene=new Arene();
+			frmArene.setVisible(true);
 		}
 		else{
 			(new ClientSocket((String)info,6666,this)).getConnexionOk();//Creation d'un objet en lui appliquant une méthode
