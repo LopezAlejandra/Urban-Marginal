c684e5abd9b2d0fe3b515ee1144f0fb5f11670b9
admin <admin@10.20.130.113>
2016-09-26 10:42:08 +0200

    Création de la classe JeuClient qui hérite de la classe Jeu

---
+++ Urban Marginal/src/modele/JeuClient.java
@@ -0,0 +1,25 @@
+package modele;
+
+import outils.connexion.Connection;
+
+public class JeuClient extends Jeu{
+
+	@Override
+	public void setConnection(Connection connection) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void reception(Connection connection, Object info) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deconnection(Connection connection) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
