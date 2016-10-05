e7ece56274f71ad36fdfa02447c9e4bb452da00d
admin <admin@10.20.130.113>
2016-10-05 15:07:10 +0200

    Variables constantes.

--- src/controleur/Global.java
+++ src/controleur/Global.java
@@ -6,6 +6,18 @@ public interface Global {
 	String CHEMIN="media"+SEPARATOR;
 	String CHEMINFONDS=CHEMIN+"fonds"+SEPARATOR;
 	String FONDCHOIX=CHEMINFONDS+"fondchoix.jpg";
+	int GAUCHE=0;
+	int DROITE=1;
+	String CHEMINPERSOS=CHEMIN+"personnages"+SEPARATOR;
+	String PERSO= CHEMINPERSOS+"perso";
+	String EXTIMAGE=".gif";
+	String MARCHE ="marche"; //les différents états
+	String BLESSE="touche";
+	String MORT="mort";
+	int NBPERSOS=3; //nombre de personnages
+	int H_PERSO=44; // taille de l'image du personnage
+	int L_PERSO=39;
+	
 	
 	
 }
