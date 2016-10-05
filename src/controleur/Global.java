c9691f00d8ce4f5b340ebffd9a45eca944f6ab3d
Alejandra Lopez <Alejandra Lopez@138.231.141.27>
2016-10-05 21:24:25 +0200

    ajout de "public static final" à chaque constante.

--- src/controleur/Global.java
+++ src/controleur/Global.java
@@ -1,22 +1,22 @@
 package controleur;
 
 public interface Global {
-	int PORT=6666;
-	String SEPARATOR = "\\";
-	String CHEMIN="media"+SEPARATOR;
-	String CHEMINFONDS=CHEMIN+"fonds"+SEPARATOR;
-	String FONDCHOIX=CHEMINFONDS+"fondchoix.jpg";
-	int GAUCHE=0;
-	int DROITE=1;
-	String CHEMINPERSOS=CHEMIN+"personnages"+SEPARATOR;
-	String PERSO= CHEMINPERSOS+"perso";
-	String EXTIMAGE=".gif";
-	String MARCHE ="marche"; //les différents états
-	String BLESSE="touche";
-	String MORT="mort";
-	int NBPERSOS=3; //nombre de personnages
-	int H_PERSO=44; // taille de l'image du personnage
-	int L_PERSO=39;
+	public static final int PORT=6666;
+	public static final String SEPARATOR = "\\";
+	public static final String CHEMIN="media"+SEPARATOR;
+	public static final String CHEMINFONDS=CHEMIN+"fonds"+SEPARATOR;
+	public static final String FONDCHOIX=CHEMINFONDS+"fondchoix.jpg";
+	public static final int GAUCHE=0;
+	public static final int DROITE=1;
+	public static final String CHEMINPERSOS=CHEMIN+"personnages"+SEPARATOR;
+	public static final String PERSO= CHEMINPERSOS+"perso";
+	public static final String EXTIMAGE=".gif";
+	public static final String MARCHE ="marche"; //les différents états
+	public static final String BLESSE="touche";
+	public static final String MORT="mort";
+	public static final int NBPERSOS=3; //nombre de personnages
+	public static final int H_PERSO=44; // taille de l'image du personnage
+	public static final int L_PERSO=39;
 	
 	
 	
