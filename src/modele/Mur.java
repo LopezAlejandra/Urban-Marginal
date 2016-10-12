e0da20dcfe1898674af9b452132357461337893a
admin <admin@10.20.130.113>
2016-10-12 11:33:26 +0200

    Constructeur

--- src/modele/Mur.java
+++ src/modele/Mur.java
@@ -1,5 +1,6 @@
 package modele;
 
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 
@@ -13,6 +14,8 @@ public class Mur extends Object implements Global {
 	super.label=new Label(-1, new JLabel());
 	super.label.getJLabel().setHorizontalAlignment(SwingConstants.CENTER);
 	super.label.getJLabel().setVerticalAlignment(SwingConstants.CENTER);
+	super.label.getJLabel().setBounds(posX, posY, L_MUR, H_MUR);
+	super.label.getJLabel().setIcon(new ImageIcon(MUR));
 	}
 	
 	
