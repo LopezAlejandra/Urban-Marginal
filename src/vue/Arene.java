1fda6405be4e8889101002e9e1feedb5eea388e6
admin <admin@10.20.130.113>
2016-10-12 09:42:41 +0200

    Construction de l'arène

--- src/vue/Arene.java
+++ src/vue/Arene.java
@@ -6,10 +6,15 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 public class Arene extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField txtSaisie;
 
 	/**
 	 * Create the frame.
@@ -17,11 +22,34 @@ public class Arene extends JFrame {
 	public Arene() {
 		setTitle("Arena");//titre de la fenêtre.
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 465, 388);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(null);
 		setContentPane(contentPane);
+		
+		JPanel jpnJeu = new JPanel();
+		jpnJeu.setBounds(286, 49, 104, 108);
+		contentPane.add(jpnJeu);
+		
+		JPanel jpnMurs = new JPanel();
+		jpnMurs.setBounds(178, 192, 215, 65);
+		contentPane.add(jpnMurs);
+		
+		JLabel lblFond = new JLabel("");
+		lblFond.setBounds(10, 22, 234, 135);
+		contentPane.add(lblFond);
+		
+		txtSaisie = new JTextField();
+		txtSaisie.setBounds(39, 270, 354, 20);
+		contentPane.add(txtSaisie);
+		txtSaisie.setColumns(10);
+		
+		JScrollPane jspChat = new JScrollPane();
+		jspChat.setBounds(39, 301, 354, 41);
+		contentPane.add(jspChat);
+		
+		JTextArea txtChat = new JTextArea();
+		jspChat.setViewportView(txtChat);
 	}
-
 }
