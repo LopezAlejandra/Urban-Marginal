e2bb515ce575d3e6d58e8d3f33b08dd166f51e0c
admin <admin@10.20.130.113>
2016-10-12 11:15:35 +0200

    Constructeur de la class Mur

--- src/modele/Mur.java
+++ src/modele/Mur.java
@@ -1,5 +1,8 @@
 package modele;
 
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
 import controleur.Global;
 
 public class Mur extends Object implements Global {
@@ -7,6 +10,18 @@ public class Mur extends Object implements Global {
 	public Mur(){
 		super.posX = (int) Math.round(Math.random() * (L_ARENE - L_MUR)) ;
 		super.posY = (int) Math.round(Math.random() * (H_ARENE - H_MUR)) ;
+	super.label=new Label(-1, new JLabel());
+	super.label.getJLabel().setHorizontalAlignment(SwingConstants.CENTER);
+	super.label.getJLabel().setVerticalAlignment(SwingConstants.CENTER);
 	}
+	
+	
+	//Toujours dans le constructeur, on va tout de suite affecter toutes les caractéristiques
+	//nécessaires au JLabel du mur (centrage, position, image). Voici comment faire :
+    //Pour centrer horizontalement le contenu d'un JLabel, il faut utiliser la méthode
+    //setHorizontalAlignment et lui envoyer en paramètre la constante statique CENTER de la
+	//classe SwingConstants. setHorizontalAlignment est une méthode qui doit s'appliquer à
+	//un JLabel, donc, en passant par la propriété label de la classe mère, accédez à son
+	//JLabel (vous avez écrit un getter pour ça).
 	
 }
