9f376ad7a392ad44f851b97789d9da2791ebc657
Alejandra Lopez <Alejandra Lopez@138.231.141.27>
2016-10-15 15:55:07 +0200

    Propriété Privée lesJoueurs du type Hashtable <Connection, Joueur>

--- src/modele/JeuServeur.java
+++ src/modele/JeuServeur.java
@@ -1,13 +1,15 @@
 package modele;
 
 import java.util.ArrayList;
+import java.util.Hashtable;
 
 import controleur.Controle;
 import controleur.Global;
 import outils.connexion.Connection;
 
 public class JeuServeur extends Jeu implements Global{
-	
+	//Le Hashtable va permettre de gérer un dictionnaire, avec une clé qui sera de type Connection.
+	private Hashtable <Connection, Joueur> lesJoueurs=new Hashtable<Connection, Joueur>();
 	private ArrayList<Mur> lesMurs=new ArrayList<Mur>();//collection des murs
 	
 	public JeuServeur(Controle controle) {
