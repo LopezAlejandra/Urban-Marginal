b1be846a03e50a955627d7c035c27d9185051f12
Alejandra Lopez <Alejandra Lopez@138.231.141.27>
2016-10-15 18:44:38 +0200

    Déclaration de deux propriétés privées: pseudo, numPerso. Création de la méthode initPerso.

--- src/modele/Joueur.java
+++ src/modele/Joueur.java
@@ -1,7 +1,15 @@
 package modele;
 import controleur.Global;
 public class Joueur extends Objet implements Global {
+	private String pseudo ;
+	private int numPerso;
+	
+	
 	public Joueur(JeuServeur jeuServeur){
+		
+	}
+	
+	public void initPerso(String unPseudo, int unNumero){
 		
 	}
 }
