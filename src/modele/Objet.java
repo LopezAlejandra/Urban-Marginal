7f49f155ce302dea418c0c19c46b41e60c3ca3d7
admin <admin@10.20.130.113>
2016-10-19 08:21:44 +0200

    Positionnement aléatoire du personnage dans l'arène: méthode toucheObjet()---contrôle l'Objet courant (quel qu'il soit) avec un autre Objet

--- src/modele/Objet.java
+++ src/modele/Objet.java
@@ -16,6 +16,30 @@ public abstract class Objet {
 	public Label getLabel() {
 		return label;
 	}
+	
+	/**
+	* contrôle si l'objet actuel touche l'objet passé en paramètre
+	* @param objet
+	* @return vrai si les 2 objets se touchent
+	*/
+	public boolean toucheObjet (Objet objet) {
+		if (objet.label==null) {
+			return false ;
+		}else{
+			if (objet.label.getjLabel()==null) {
+				return false ;
+		
+			}else{
+				int l_obj = objet.label.getjLabel().getWidth() ;
+				int h_obj = objet.label.getjLabel().getHeight() ;
+				int l_this = this.label.getjLabel().getWidth() ;
+				int h_this = this.label.getjLabel().getHeight() ;
+				return(!((this.posX+l_this<objet.posX ||
+						this.posX>objet.posX+l_obj) ||
+						(this.posY+h_this<objet.posY ||
+						this.posY>objet.posY+h_obj))) ;
+			}
+		}
+	}
 
-
-}
+}	
