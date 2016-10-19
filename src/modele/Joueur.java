560d03142bc12b632d4b52d4ceb10d984df4436e
admin <admin@10.20.130.113>
2016-10-19 15:54:53 +0200

    suppression de la variable personnage appel de la méthode nouveauLabelJeu sur l'objet jeuServeur"

--- src/modele/Joueur.java
+++ src/modele/Joueur.java
@@ -1,19 +1,16 @@
 package modele;
-import java.awt.Component;
 import java.awt.Font;
-
 import java.util.ArrayList;
 import java.util.Hashtable;
-
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import controleur.Global;
 import outils.connexion.Connection;
+
 public class Joueur extends Objet implements Global {
 	private String pseudo ;
 	private int numPerso;
-	private Label personnage;
 	private Label message;// pour l'affichage du pseudo et de la vie
 	private JeuServeur jeuServeur;
 	private int vie;//vie restante du joueur
@@ -36,20 +33,19 @@ public class Joueur extends Objet implements Global {
 		this.pseudo=unPseudo;
 		this.numPerso=unNumero;
 		label=new Label(Label.getNbLabel()+1,new JLabel());//contient le personnage
+		Label.setNbLabel(Label.getNbLabel() + 1);
+		jeuServeur.nouveauLabelJeu(label);
 		label.getjLabel().setHorizontalAlignment(SwingConstants.CENTER);
 		label.getjLabel().setVerticalAlignment(SwingConstants.CENTER);
-		jeuServeur.nouveauLabelJeu(label);
 		
 		message = new Label(Label.getNbLabel()+1, new JLabel());
 		message.getjLabel().setHorizontalAlignment(SwingConstants.CENTER);
 		message.getjLabel().setFont(new Font("Dialog", Font.PLAIN, 8));
-	
-		premierePosition(lesJoueurs, lesMurs);
 		jeuServeur.nouveauLabelJeu(message);
+		premierePosition(lesJoueurs, lesMurs);
+		
 		affiche(MARCHE,etape);
 	}
-	
-	
 
 	private boolean toucheJoueur( Hashtable <Connection, Joueur> lesJoueurs){//reçoit en
 																				//paramètre lesJoueurs (le dictionnaire de joueurs).
@@ -67,7 +63,7 @@ public class Joueur extends Objet implements Global {
 	private boolean toucheMur(ArrayList <Mur>lesMurs){
 		for (Mur unMur:lesMurs){
 			if(super.toucheObjet(unMur)){
-				return true;//il y a collision,	
+				return true;
 			}
 		}
 		return false;
@@ -76,18 +72,16 @@ public class Joueur extends Objet implements Global {
 	private void premierePosition(Hashtable<Connection,Joueur>lesJoueurs,ArrayList <Mur>lesMurs){
 		label.getjLabel().setBounds(0, 0, L_PERSO, H_PERSO);//affecter une taille au JLabel du personnage
 		do{
-			super.posX = (int)Math.round(Math.random() * (L_ARENE-L_PERSO)) ; 
-			super.posY = (int)Math.round(Math.random() * (H_ARENE-H_PERSO-H_MESSAGE)) ; 
+			super.posX = (int)Math.round(Math.random()* (L_ARENE-L_PERSO)) ; 
+			super.posY = (int)Math.round(Math.random()* (H_ARENE-H_PERSO-H_MESSAGE)) ; 
 		}
 		while(toucheJoueur(lesJoueurs)||toucheMur(lesMurs));//tant quel'instance actuelle touche un mur ou un autre joueur.
-
-	
 	}
 	
-	public void affiche(String etat,int etape){//String qui contiendrq l'état d'un joueur (marche,blessé,mort) et entier qui contiendra le num d'etape
+	public void affiche(String etat,int etape){//String qui contiendra l'état d'un joueur (marche,blessé,mort) et entier qui contiendra le num d'etape
 		label.getjLabel().setBounds(posX, posY, L_PERSO, H_PERSO);
 		label.getjLabel().setIcon(new ImageIcon(PERSO+this.numPerso+etat+etape+"d"+orientation+EXTIMAGE));
-		message.getjLabel().setBounds(posX-10, posY+H_PERSO, L_PERSO+20, H_MESSAGE);
+		message.getjLabel().setBounds(posX-10, posY, L_PERSO+0, H_MESSAGE);
 		message.getjLabel().setText(pseudo+" : "+vie);
 		
 	}
