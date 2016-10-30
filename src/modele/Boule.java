d86464876de32af46bde527586cc2839e43152ab
Alejandra Lopez <Alejandra Lopez@138.231.141.27>
2016-10-30 23:06:18 +0100

    Constructeur de la classe Boule qui initialise le Label correspondant. Le label est centrè horizontalement et verticalement

--- src/modele/Boule.java
+++ src/modele/Boule.java
@@ -1,5 +1,15 @@
 package modele;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
 import controleur.Global;
 public class Boule extends Objet implements Global {
-
+	
+	//constructeur
+	public Boule(){
+		super.label=new Label(Label.nbLabel++, new JLabel());
+		super.label.getjLabel().setHorizontalAlignment(SwingConstants.CENTER);
+		super.label.getjLabel().setVerticalAlignment(SwingConstants.CENTER);
+		
+	}
 }
