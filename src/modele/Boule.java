01a9378e445dd393a5287aa00ba3d1e1702c2997
Alejandra Lopez <Alejandra Lopez@138.231.141.27>
2016-10-30 23:18:36 +0100

    Intégration du JLabel de la boule dans le JPanel: -Déclaration de la prop. privée jeuServeur -Valorisation de cette propriété dans le constructeur -Appel de la méthode nouveauLabelJeu sur la propriété jeuServeur

--- src/modele/Boule.java
+++ src/modele/Boule.java
@@ -5,14 +5,18 @@ import javax.swing.SwingConstants;
 
 import controleur.Global;
 public class Boule extends Objet implements Global {
+	private JeuServeur jeuServeur;
 	
 	//constructeur
-	public Boule(){
+	public Boule(JeuServeur jeuServeur){
+		this.jeuServeur=jeuServeur;
 		super.label=new Label(Label.nbLabel++, new JLabel());
 		super.label.getjLabel().setHorizontalAlignment(SwingConstants.CENTER);
 		super.label.getjLabel().setVerticalAlignment(SwingConstants.CENTER);
 		super.label.getjLabel().setBounds(0, 0, L_BOULE, H_BOULE);
 		super.label.getjLabel().setIcon(new ImageIcon(BOULE));
 		super.label.getjLabel().setVisible(false);
+		
+		jeuServeur.nouveauLabelJeu(super.label);//Ainsi le JLabel sera ajoutè au JPanel de Serveur
 	}
 }
