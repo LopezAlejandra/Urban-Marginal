de1351755597a6ddf10bb4dc8c4e1b1834e81f41
Alejandra Lopez <Alejandra Lopez@138.231.141.27>
2016-10-30 23:11:21 +0100

    Nouvelles constantes qui vont permettre d'affecter l'image de la boule

--- src/controleur/Global.java
+++ src/controleur/Global.java
@@ -41,5 +41,7 @@ public interface Global {
 	public static final int LEPAS=10;//va permettre de faire avancer ou reculer le personnage
 	public static final int L_BOULE=17;
 	public static final int H_BOULE=17;
+	public static final String CHEMINBOULES=CHEMIN+"boules"+SEPARATOR;
+	public static final String BOULE=CHEMINBOULES+"boule.gif";
 	
 }
