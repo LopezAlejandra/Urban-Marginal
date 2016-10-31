d9c24983c447125daf879149bf68befa09d294bb
Alejandra Lopez <Alejandra Lopez@138.231.141.27>
2016-10-31 11:05:54 +0100

    Boucle qui va permettre de boucler sur l'avancée de la boule: -Modification des posX et posY -Test sur orientation -Modification de la position du JLabel avec setBounds -Envoi à tous les joueurs -Condition de la boucle -Rendre invisible la boule

--- src/modele/Attaque.java
+++ src/modele/Attaque.java
@@ -10,4 +10,25 @@ public class Attaque extends Thread implements Global{
 		this.jeuServeur=jeuServeur;
 		super.start();//va permettre de lancer le processus
 	}
+	
+	public void run(){
+		Boule laboule=attaquant.getBoule();
+		int orientation=attaquant.getOrientation();
+		laboule.label.getjLabel().setVisible(true);
+		
+		do{
+			if(orientation==GAUCHE){
+				laboule.setPosX(laboule.getPosX()-LEPAS);
+			}else{
+				laboule.setPosX(laboule.getPosX()+LEPAS);
+			}
+			laboule.label.getjLabel().setBounds(laboule.getPosX(), laboule.getPosY(), L_BOULE, H_BOULE);
+			jeuServeur.envoi(laboule.getLabel());//envoi de la position à tous les joueurs
+			
+		}
+		while(laboule.getPosX()>0 &&laboule.getPosX()<L_ARENE);
+		laboule.getLabel().getjLabel().setVisible(false);
+		jeuServeur.envoi(laboule.getLabel());
+	}
+	
 }
