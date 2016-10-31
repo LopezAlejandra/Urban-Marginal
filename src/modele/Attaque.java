a07dd6d63e84297b221ac57baa2855ce3feee6b8
Alejandra Lopez <Alejandra Lopez@138.231.141.27>
2016-10-31 10:41:07 +0100

    Constructeur page 53

--- src/modele/Attaque.java
+++ src/modele/Attaque.java
@@ -4,4 +4,10 @@ import controleur.Global;
 public class Attaque extends Thread implements Global{
 	private Joueur attaquant;
 	private JeuServeur jeuServeur;
+	
+	public Attaque(Joueur attaquant, JeuServeur jeuServeur){
+		this.attaquant=attaquant;
+		this.jeuServeur=jeuServeur;
+		super.start();//va permettre de lancer le processus
+	}
 }
