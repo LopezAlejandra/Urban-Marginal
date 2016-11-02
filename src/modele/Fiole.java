d44eaca24575df6fc67ebf6eae0825a3cc736c9d
Alejandra Lopez <Alejandra Lopez@138.231.141.27>
2016-11-02 11:27:03 +0100

    Modification dans le constructeur

--- src/modele/Fiole.java
+++ src/modele/Fiole.java
@@ -8,21 +8,21 @@ import javax.swing.SwingConstants;
 import controleur.Global;
 
 public class Fiole extends Objet implements Global {
-	private JeuServeur jeuServeur;
+	
 	
 	public Fiole(){
 		
-		this.jeuServeur=jeuServeur;
+		
 		super.posX =(int) Math.round(Math.random() * (L_ARENE - L_FIOLE)) ;
 		super.posY =(int) Math.round(Math.random() * (H_ARENE - H_FIOLE));
-		super.label=new Label(Label.nbLabel++, new JLabel());
-		
+		super.label= new Label(-1,new JLabel ());
 		super.label.getjLabel().setHorizontalAlignment(SwingConstants.CENTER) ;//Pour centrer horizontalement le contenu 
 		super.label.getjLabel().setVerticalAlignment(SwingConstants.CENTER) ;//Pour centrer verticalement le contenu 
 		super.label.getjLabel().setBounds(new Rectangle (posX, posY, L_FIOLE, H_FIOLE));
 		super.label.getjLabel().setIcon(new ImageIcon(FIOLE));
 		
-		jeuServeur.nouveauLabelJeu(super.label);//Ainsi le JLabel sera ajoutè au JPanel de Serveur
+		
+		
 	}
 	
 	
